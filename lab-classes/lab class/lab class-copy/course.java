e97fe58c6ba037c6fbeb27cc9aeca29cfd877cb6
leviward325 <71874129+leviward325@users.noreply.github.com>
2020-10-17 16:52:57 +0100

    lab class

---
+++ lab-classes/lab class/lab class-copy/course.java
@@ -0,0 +1,29 @@
+
+/**
+ * Write a description of class course here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class course
+{
+// instance variables - replace the example below with your own
+private String codeNo;
+private String title;
+/**
+* Constructor for objects of class course
+*/
+public course(String codeNo, String title)
+{
+ // initialise instance variables
+ this.codeNo = codeNo;
+ this.title = title;
+}
+
+/**
+* An example of a method - replace this comment with your own
+*
+* @param  y  a sample parameter for a method
+* @return    the sum of x and y
+*/
+
